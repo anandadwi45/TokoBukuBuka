3999798e24889f851250bbbc6718f8996f54f107
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2020-03-28 02:50:30 +0800

    db from azure

--- app/src/main/java/com/adp/tokobukubuka/Server.java
+++ app/src/main/java/com/adp/tokobukubuka/Server.java
@@ -1,5 +1,5 @@
 package com.adp.tokobukubuka;
 
 public class Server {
-    public static final String URL = "https://tokobukubuka.000webhostapp.com/";
+    public static final String URL = "https://tokobukuapp.azurewebsites.net/";
 }
