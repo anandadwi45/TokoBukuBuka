e115198a9d236a9f4cfd388c294d7ecb6dd6fa35
anandadwi45 <anandadwi45@gmail.com>
2020-03-28 23:03:41 +0800

    PDF Viewer

--- app/src/main/java/com/adp/tokobukubuka/ViewerActivity.java
+++ app/src/main/java/com/adp/tokobukubuka/ViewerActivity.java
@@ -1,13 +1,55 @@
 package com.adp.tokobukubuka;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ViewerActivity extends AppCompatActivity {
+
+    PDFView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewer);
+
+        pdfView = (PDFView) findViewById(R.id.pdfView);
+
+        new RetrievePDFStream().execute("https://media.neliti.com/media/publications/233736-studi-implementasi-sistem-big-data-untuk-857db2bd.pdf");
+
+    }
+
+    class RetrievePDFStream extends AsyncTask<String,Void, InputStream>
+    {
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            InputStream inputStream=null;
+            try{
+                URL url = new URL(strings[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+                if(urlConnection.getResponseCode() == 200){
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                }
+            }
+            catch (IOException e)
+            {
+                return null;
+            }
+            return inputStream;
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            pdfView.fromStream(inputStream).load();
+        }
     }
 }
